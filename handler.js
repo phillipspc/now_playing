9f3bfd73c8b43e832948ad8f684084d42ee18019
Peter Phillips <peter.phillips@rtslabs.com>
2018-05-30 21:21:14 -0400

    use SNS to kick off 'dispatcher' lambda

--- handler.js
+++ handler.js
@@ -1,11 +1,29 @@
 'use strict';
 const queryString = require('query-string');
 const service = require('./service');
+const AWS = require('aws-sdk');
+const sns = new AWS.SNS();
 
 module.exports.nowPlaying = (event, context, callback) => {
   let parsed = queryString.parse(event.body);
   let id = parsed.user_id + "-" + parsed.team_id;
   let responseUrl = parsed.response_url;
+
+  sns.publish({
+    Message: JSON.stringify({id: id, responseUrl: responseUrl}),
+    TopicArn: "arn:aws:sns:us-east-1:117620298113:dispatch"
+  }, function(err, data) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  callback(null, { statusCode: 200 });
+};
+
+module.exports.dispatcher = (event, context, callback) => {
+  let parsed = JSON.parse(event.Records[0].Sns.Message);
+  let id = parsed.id;
+  let responseUrl = parsed.responseUrl;
 
   let queryParams = {
     TableName: "users",
@@ -18,7 +36,7 @@ module.exports.nowPlaying = (event, context, callback) => {
     }
   };
 
-  service.findUser(queryParams, id, responseUrl, callback);
+  service.findUser(queryParams, id, responseUrl);
 };
 
 module.exports.callback = (event, context, callback) => {
--- service.js
+++ service.js
@@ -16,9 +16,7 @@ module.exports = {
   createUser: function(params, callback) {
     dynamo.put(params, (error, data) => {
       if (error) {
-        console.error(params);
-        callback(new Error('Error when creating user.'));
-        return;
+        console.log("Error creating user", error);
       }
 
       let response = {
@@ -29,32 +27,35 @@ module.exports = {
     })
   },
 
-  findUser: function(params, id, responseUrl, callback) {
+  findUser: function(params, id, responseUrl) {
     dynamo.query(params, (error, data) => {
       if (!error && data.Count === 1) {
         this.showNowPlaying(data.Items[0], responseUrl);
-        callback(null);
       } else {
-        this.sendAuthUrl(id, callback);
+        this.sendAuthUrl(id, responseUrl);
       }
     });
   },
 
-  sendAuthUrl: function (id, callback) {
+  sendAuthUrl: function (id, responseUrl) {
     let scopes = ['user-read-playback-state'];
     let state = id;
     let url = spotifyApi.createAuthorizeURL(scopes, state);
 
-    let response = {
-      statusCode: 200,
-      body: url
-    };
+    slack.setWebhook(responseUrl);
 
-    callback(null, response);
+    slack.webhook({
+      text: url
+    }, function(err, response) {
+      if (err) {
+        console.log("Error posting Auth URL via webhook", err);
+      }
+    });
   },
 
   createUserWithTokens: function (code, params, callback) {
     let service = this;
+
     spotifyApi.authorizationCodeGrant(code).then(
       function (data) {
         params.Item.refresh_token = data.body['refresh_token'];
@@ -68,25 +69,35 @@ module.exports = {
 
   showNowPlaying: function (user, responseUrl) {
     let refreshToken = user.refresh_token;
+    slack.setWebhook(responseUrl);
+
     spotifyApi.setRefreshToken(refreshToken);
     spotifyApi.refreshAccessToken().then(
       function (data) {
         spotifyApi.setAccessToken(data.body['access_token']);
         spotifyApi.getMyCurrentPlaybackState({}).then(
           function (data) {
-            let spotifyUrl = data.body.item.external_urls.spotify;
-            slack.setWebhook(responseUrl);
+            if (Object.keys(data.body).length !== 0) {
+              let spotifyUrl = data.body.item.external_urls.spotify;
 
-            slack.webhook({
-              text: spotifyUrl,
-              response_type: "in_channel",
-              unfurl_links: true
-            }, function(err, response) {
-              if (err) {
-                console.log("Error posting to webhook", err);
-              }
-            });
-
+              slack.webhook({
+                text: spotifyUrl,
+                response_type: "in_channel",
+                unfurl_links: true
+              }, function(err, response) {
+                if (err) {
+                  console.log("Error posting to webhook", err);
+                }
+              });
+            } else {
+              slack.webhook({
+                text: "It doesnt look like you're currently listening to anything."
+              }, function(err, response) {
+                if (err) {
+                  console.log("Error posting to webhook", err);
+                }
+              });
+            }
           }, function (err) {
             console.log("Error when getting current playback", err);
           }
